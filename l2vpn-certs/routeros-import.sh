a106cb7220618ef2b88da42e6c646cb500b802b1
Jody Lemoine <jody.lemoine@tishco.ca>
2026-03-13 01:19:57 -0400

    Adjusted configurations and presentation to remove unnecessary spoke certificates.
    Changes to be committed:
    	modified:   MTPC 2026 Presentation.pdf
    	modified:   l2vpn-certs/routeros-import.sh
    	modified:   l2vpn-certs/spokes.rsc
    	modified:   l2vpn-l2tp-ipsec/spoke1.rsc
    	modified:   l2vpn-l2tp-ipsec/spoke2.rsc
    	modified:   l2vpn-l2tp-ipsec/spoke3.rsc
    	modified:   l2vpn-l2tp-ipsec/spoke4.rsc

--- l2vpn-certs/routeros-import.sh
+++ l2vpn-certs/routeros-import.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/zsh
+#
 CA=$1
 NAME=$2
 echo '/file' > $NAME.rsc
 cat $1.crt >> $NAME.rsc
 sed -i "s/^-----BEGIN CERTIFICATE-----$/add name=$1.crt type=file contents=\"&/" $NAME.rsc
-cat $NAME.crt $NAME.key >> $NAME.rsc
-sed -i "s/^-----BEGIN CERTIFICATE-----$/add name=$NAME.crt type=file contents=\"&/" $NAME.rsc
-sed -i "s/^-----BEGIN PRIVATE KEY-----$/add name=$NAME.key type=file contents=\"&/" $NAME.rsc
+if [ -f $NAME.key ]; then
+  cat $NAME.crt $NAME.key >> $NAME.rsc
+  sed -i "s/^-----BEGIN CERTIFICATE-----$/add name=$NAME.crt type=file contents=\"&/" $NAME.rsc
+  sed -i "s/^-----BEGIN PRIVATE KEY-----$/add name=$NAME.key type=file contents=\"&/" $NAME.rsc
+fi
 sed -i '2,$s/$/\\n/' $NAME.rsc && sed -i 's/^\(.*END.*\)\\n/\1"/' $NAME.rsc
 echo '/certificate; import; :foreach counter=entry in=[find] do={set $entry name=[get $entry common-name]}' >> $NAME.rsc
